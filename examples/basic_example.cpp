1e96d4e22c38176fff2f7552c9e037a8ae7870b1
ASOBIBA <86977637+LHBM04@users.noreply.github.com>
2026-02-05 11:10:58 +0900

    Update basic_example.cpp
    - 기존에 있던 스켈레톤 코드에서 간단한 코드로 변경.
    - Type alias 테스트 코드.

--- examples/basic_example.cpp
+++ examples/basic_example.cpp
@@ -1,4 +1,14 @@
-int main(int, char**);
+#include <misc/macros.hpp>
+#include <misc/types.hpp>
+
+#include <iostream>
+
+using namespace amuse::core;
+
+int main(int argc_, char** argv_)
 {
-    return 0;
+    constexpr Int32 lhs = 10;
+    constexpr Int32 rhs = 20;
+
+    std::cout << "Sum of " << lhs << " and " << rhs << " is " << (lhs + rhs) << std::endl;
 }
